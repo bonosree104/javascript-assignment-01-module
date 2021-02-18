3752915244fa7b7fe44e166da5c0e0ca5641f9a1
bonosree104 <bonosreeela104@gmail.com>
2021-02-18 10:01:34 +0600

    tinyFriend.js comitted

---
+++ tinyFriend.js
@@ -0,0 +1,15 @@
+
+    var friends = ['toma','tonny','doi','chira'];
+    var min = friends[0];
+    for (i=0; i <friends.length; i++){
+        var element = friends [i];
+        if (element< min){
+            min=element;
+        }
+    
+        }
+    
+    
+
+
+console.log("minimum element is:", min);
