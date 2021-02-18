210efcec31189a6c20ea5dbdc6b287698bbfe620
bonosree104 <bonosreeela104@gmail.com>
2021-02-18 10:50:44 +0600

    initial commited

---
+++ woodCalculator.js
@@ -0,0 +1,27 @@
+function doubleIt(num){
+    var result =num*2;
+    console.lnog(result);
+}
+//chair,table,bed//
+ doubleIt (1);
+  doubleIt (3);
+ doubleIt(5);
+
+// total wood//
+ function doubleIt(num){
+     var result =num *2;
+     return result;
+ }
+ var chair = doubleIt(1);
+ var table = doubleIt(3);
+ var bed = doubleIt(5);
+ var total =chair+table+bed;
+ console.log(total);
+
+// aro chair table,bed er jonno wood//
+ function add (num1,num2, num3){
+     var result = num1+num2+num3;
+     return result;
+ }
+ var sum = add (15, 16, 10);
+ console.log(sum);
