275be09375f957c0f191f1642dc10a2488a7e4ea
bonosree104 <bonosreeela104@gmail.com>
2021-02-18 08:01:36 +0600

    first commit

---
+++ feetToMile.js
@@ -0,0 +1,8 @@
+function feetToMile (feet) {
+    var mile = feet/5280;
+    return mile;
+}
+var nanamile = feetToMile (47520);
+console.log (nanamile);
+var nanimile = feetToMile (10560);
+console.log(nanimile);
